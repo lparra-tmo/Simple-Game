3cd433fbe19e52b4e74ee066d059d909a14d317d
Luis Carlos Parra Mendoza <lparra@pkglobal.com>
2021-03-17 15:37:50 -0700

    commit

---
+++ Assets/CoinScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    public AudioSource _as;
+    public AudioClip collectSound;
+
+
+
+
+    void Start(Collider2D col)
+    {
+        _as = GetComponent<AudioSource>();
+    }
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+       
+       _as.PlayOneShot(collectSound);
+     
+        Destroy(gameObject);
+    
+
+    }
+
+
+}
